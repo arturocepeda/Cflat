87817f24d6de2301efad897a85d9ff3f91ecf8ac
Arturo Cepeda <arturocepedaperez@gmail.com>
2019-04-05 11:32:17 +0200

    Added cpp file with unit tests (Google Test)

---
+++ tests/tests.cpp
@@ -0,0 +1,166 @@
+
+#include "gtest/gtest.h"
+
+#include "../Cflat.h"
+
+TEST(Cflat, VariableDeclaration)
+{
+   Cflat::Environment env;
+
+   const char* code =
+      "int var = 42;";
+
+   env.load(code);
+
+   int var = CflatRetrieveValue(env.getVariable("var"), int,,);
+   EXPECT_EQ(var, 42);
+}
+
+TEST(Cflat, VariableAssignment)
+{
+   Cflat::Environment env;
+
+   const char* code =
+      "int var;"
+      "var = 42;";
+
+   env.load(code);
+
+   int var = CflatRetrieveValue(env.getVariable("var"), int,,);
+   EXPECT_EQ(var, 42);
+}
+
+TEST(Cflat, StdStringUsage)
+{
+   Cflat::Environment env;
+
+   {
+      CflatRegisterClass(&env, std::string);
+      CflatClassAddConstructor(&env, std::string);
+      CflatClassAddMethodReturnParams1(&env, std::string, std::string,&,*, assign, const char*,,);
+   }
+
+   const char* code =
+      "std::string str;\n"
+      "str.assign(\"Hello world!\");\n";
+
+   env.load(code);
+
+   std::string& str = CflatRetrieveValue(env.getVariable("str"), std::string,,);
+   EXPECT_TRUE(strcmp(str.c_str(), "Hello world!") == 0);
+}
+
+TEST(Cflat, MemberAssignment)
+{
+   Cflat::Environment env;
+
+   struct TestStruct
+   {
+      int var1;
+      int var2;
+   };
+
+   {
+      CflatRegisterStruct(&env, TestStruct);
+      CflatStructAddMember(&env, TestStruct, int, var1);
+      CflatStructAddMember(&env, TestStruct, int, var2);
+      CflatStructAddConstructor(&env, TestStruct);
+   }
+
+   const char* code =
+      "TestStruct testStruct;\n"
+      "testStruct.var1 = 42;\n"
+      "testStruct.var2 = 100;\n";
+
+   env.load(code);
+
+   TestStruct& testStruct = CflatRetrieveValue(env.getVariable("testStruct"), TestStruct,,);
+   EXPECT_EQ(testStruct.var1, 42);
+   EXPECT_EQ(testStruct.var2, 100);
+}
+
+TEST(Cflat, MemberAssignmentPointer)
+{
+   Cflat::Environment env;
+
+   struct TestStruct
+   {
+      int var1;
+      int var2;
+   };
+
+   {
+      CflatRegisterStruct(&env, TestStruct);
+      CflatStructAddMember(&env, TestStruct, int, var1);
+      CflatStructAddMember(&env, TestStruct, int, var2);
+      CflatStructAddConstructor(&env, TestStruct);
+   }
+
+   const char* code =
+      "TestStruct testStruct;\n"
+      "TestStruct* testStructPtr = &testStruct;\n"
+      "testStructPtr->var1 = 42;\n"
+      "testStructPtr->var2 = 100;\n";
+
+   env.load(code);
+
+   TestStruct& testStruct = CflatRetrieveValue(env.getVariable("testStruct"), TestStruct,,);
+   EXPECT_EQ(testStruct.var1, 42);
+   EXPECT_EQ(testStruct.var2, 100);
+}
+
+TEST(Cflat, VoidMethodCallNoParams)
+{
+   Cflat::Environment env;
+
+   struct TestStruct
+   {
+      int var;
+
+      TestStruct() : var(0) {}
+      void method() { var = 42; }
+   };
+
+   {
+      CflatRegisterStruct(&env, TestStruct);
+      CflatStructAddConstructor(&env, TestStruct);
+      CflatStructAddMethodVoid(&env, TestStruct, void,,, method);
+   }
+
+   const char* code =
+      "TestStruct testStruct;\n"
+      "testStruct.method();\n";
+
+   env.load(code);
+
+   TestStruct& testStruct = CflatRetrieveValue(env.getVariable("testStruct"), TestStruct,,);
+   EXPECT_EQ(testStruct.var, 42);
+}
+
+TEST(Cflat, VoidMethodCallWithParam)
+{
+   Cflat::Environment env;
+
+   struct TestStruct
+   {
+      int var;
+
+      TestStruct() : var(0) {}
+      void method(int val) { var = val; }
+   };
+
+   {
+      CflatRegisterStruct(&env, TestStruct);
+      CflatStructAddConstructor(&env, TestStruct);
+      CflatStructAddMethodVoidParams1(&env, TestStruct, void,,, method, int,,);
+   }
+
+   const char* code =
+      "TestStruct testStruct;\n"
+      "testStruct.method(42);\n";
+
+   env.load(code);
+
+   TestStruct& testStruct = CflatRetrieveValue(env.getVariable("testStruct"), TestStruct,,);
+   EXPECT_EQ(testStruct.var, 42);
+}
