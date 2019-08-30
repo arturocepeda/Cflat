20c9a13af32eab68275cffa5454402b99107f9ef
Arturo Cepeda <acepeda@deck13.com>
2019-08-30 15:04:13 +0200

    Make sure that the current token index is correct after having parsed a 'do-while' statement

--- Cflat.cpp
+++ Cflat.cpp
@@ -3260,6 +3260,7 @@ StatementDoWhile* Environment::parseStatementDoWhile(ParsingContext& pContext)
    tokenIndex++;
 
    Expression* condition = parseExpression(pContext, conditionClosureTokenIndex - 1u);
+   tokenIndex = conditionClosureTokenIndex + 1u;
 
    StatementDoWhile* statement = (StatementDoWhile*)CflatMalloc(sizeof(StatementDoWhile));
    CflatInvokeCtor(StatementDoWhile, statement)(condition, loopStatement);
