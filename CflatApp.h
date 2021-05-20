c872e565e3042e4fff0484bc5bae7ef3ddc67f43
Arturo Cepeda <acepeda@deck13.com>
2021-05-20 09:09:56 +0200

    Added the 'CflatApp' headers

---
+++ CflatApp.h
@@ -0,0 +1,138 @@
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Cflat v0.20
+//  Embeddable lightweight scripting language with C++ syntax
+//
+//  Copyright (c) 2019-2021 Arturo Cepeda Pérez
+//
+//  ---------------------------------------------------------------------------
+//
+//  This software is provided 'as-is', without any express or implied
+//  warranty. In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//     claim that you wrote the original software. If you use this software
+//     in a product, an acknowledgment in the product documentation would be
+//     appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be
+//     misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "CflatAppConfig.h"
+
+#if defined CFLAT_ENABLED
+
+//
+//  Forward declarations
+//
+namespace Cflat
+{
+   class Environment;
+   struct Function;
+}
+
+
+//
+//  Function declarations (must be implemented on the client side)
+//
+namespace CflatApp
+{
+   extern Cflat::Environment* getGlobalEnvironment();
+
+   extern void lockGlobalEnvironment();
+   extern void unlockGlobalEnvironment();
+
+   extern void onError(const char* pErrorMessage);
+}
+
+
+//
+//  Script inclusion
+//
+# define CflatScript(pScript)  <CflatHeadersPath/Cflat.h>
+
+
+//
+//  Thread-safety
+//
+# define CflatLock()  CflatApp::lockGlobalEnvironment();
+# define CflatUnlock()  CflatApp::unlockGlobalEnvironment();
+
+//
+//  Value retrieval
+//
+# define CflatGet(pType, pIdentifier) \
+   CflatValueAs(CflatApp::getGlobalEnvironment()->getVariable(#pIdentifier), pType)
+# define CflatGetArray(pElementType, pIdentifier) \
+   CflatValueAsArray(CflatApp::getGlobalEnvironment()->getVariable(#pIdentifier), pElementType)
+
+//
+//  Function calls
+//
+# define CflatArg(pArg) &pArg
+# define CflatVoidCall(pFunction, ...) \
+   { \
+      Cflat::Environment* env = CflatApp::getGlobalEnvironment(); \
+      Cflat::Function* function = env->getFunction(#pFunction); \
+      if(function) \
+      { \
+         env->voidFunctionCall(function, __VA_ARGS__); \
+         if(env->getErrorMessage()) \
+         { \
+            CflatApp::onError(env->getErrorMessage()); \
+         } \
+      } \
+   }
+# define CflatReturnCall(pLValue, pReturnType, pFunction, ...) \
+   { \
+      Cflat::Environment* env = CflatApp::getGlobalEnvironment(); \
+      Cflat::Function* function = env->getFunction(#pFunction); \
+      if(function) \
+      { \
+         pLValue = env->returnFunctionCall<pReturnType>(function, __VA_ARGS__); \
+         if(env->getErrorMessage()) \
+         { \
+            CflatApp::onError(env->getErrorMessage()); \
+         } \
+      } \
+   }
+
+#else
+
+//
+//  Script inclusion
+//
+# define CflatScript(pScript)  <CflatScriptsPath/pScript>
+
+//
+//  Thread-safety
+//
+# define CflatLock()
+# define CflatUnlock()
+
+//
+//  Value retrieval
+//
+# define CflatGet(pType, pIdentifier)  pIdentifier
+# define CflatGetArray(pElementType, pIdentifier)  pIdentifier
+
+//
+//  Function calls
+//
+# define CflatArg(pArg)  pArg
+# define CflatVoidCall(pFunction, ...)  pFunction(__VA_ARGS__)
+# define CflatReturnCall(pLValue, pReturnType, pFunction, ...)  pLValue = pFunction(__VA_ARGS__)
+
+#endif
