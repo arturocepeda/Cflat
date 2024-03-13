61debea33a7215fbaba7998c18e4cdcdcffac6b0
mundusnine <mundusnine@gmail.com>
2024-03-13 14:00:10 -0400

    Fix copyright name

--- Cflat.cpp
+++ Cflat.cpp
@@ -4,7 +4,7 @@
 //  Cflat v0.60
 //  Embeddable lightweight scripting language with C++ syntax
 //
-//  Copyright (c) 2019-2024 Arturo Cepeda P�rez and contributors
+//  Copyright (c) 2019-2024 Arturo Cepeda Pérez and contributors
 //
 //  ---------------------------------------------------------------------------
 //
