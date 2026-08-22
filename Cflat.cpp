e2b12d5db9295b6cac3fa193aca8ada13a7e4a6a
Arturo Cepeda <arturocepedaperez@gmail.com>
2026-08-22 06:29:43 +0200

    Disabled 'strncpy' unsafe warnings on MSVC++

--- Cflat.cpp
+++ Cflat.cpp
@@ -28,6 +28,12 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+
+#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
+# define _CRT_SECURE_NO_WARNINGS
+#endif
+
+
 #include "Cflat.h"
 #include <cmath>
 
